b7db080b7aa59e107834953f0a01492a834ab16c
Joshwawo <masacota_masacota@hotmail.com>
2022-11-06 00:27:49 -0700

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import {BrowserRouter} from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Lexica from "./pages/Lexica";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   //no tienes snippets?
   return (
     <div className="App">
       <BrowserRouter>
-
-
-
+        <Routes>
+          <Route path="/" element={<Lexica />} />
+        </Routes>
       </BrowserRouter>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Lexica.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lexica = () => {
+  return (
+    <>
+     <p>Hola desde la pagina de lexica</p>   
+    </>
+  )
+}
+
+export default Lexica
